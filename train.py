2db74b134c649fa0a2091134a882f886bcb9fecd
lyz27 <1156286458@qq.com>
2024-01-26 19:13:00 +0800

    2024年1月26号进行的魔鬼面具的第一次上传

---
+++ train.py
@@ -0,0 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/yolov8n.yaml')
+    model.load('yolov8n.pt') # loading pretrain weights
+    model.train(data='dataset/data.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=100,
+                batch=16,
+                close_mosaic=10,
+                workers=4,
+                device='0',
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False, # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
