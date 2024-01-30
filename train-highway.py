970a288ed8d9c145d59640db36d3eed93218060d
lyz27 <1156286458@qq.com>
2024-01-31 01:03:03 +0800

    进行小目标训练的文件修改和添加

---
+++ train-highway.py
@@ -0,0 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/lyz-change/yolov8-seg-EfficientHead.yaml')
+    model.load('yolov8n-seg.pt') # loading pretrain weights
+    model.train(data='dataset/data-highway.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=100,
+                batch=8,
+                close_mosaic=10,
+                workers=4,
+                device='cpu',
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False, # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
