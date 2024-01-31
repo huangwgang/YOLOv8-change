84b102c6d339dcb47713c6e4ce8369cf6a7a5258
lyz27 <1156286458@qq.com>
2024-01-31 11:15:00 +0800

    goldyolo + seg

--- train-highway.py
+++ train-highway.py
@@ -3,12 +3,12 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/lyz-change/yolov8-seg-EfficientHead.yaml')
+    model = YOLO('ultralytics/cfg/models/lyz-change/yolov8-goldyolo-seg.yaml')
     model.load('yolov8n-seg.pt') # loading pretrain weights
     model.train(data='dataset/data-highway.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=100,
+                epochs=10,
                 batch=8,
                 close_mosaic=10,
                 workers=4,
